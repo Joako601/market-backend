d10a20fa1447b7405b918a5d99c649f9bf591502
Joako601 <joaquin.uriona@tecdesoftware.edu.mx>
2026-06-02 08:53:50 -0600

    feat: implement ProductoRepository and custom query methods - Add custom query methods in ProductoCrudRepository (sorted category filter and low stock check).- Create ProductoRepository to wrap CRUD operations (getAll, getByCategoria, getEscasos, getProductoById, save).

--- src/main/java/mx/edu/tecdesoftware/market_backend/persistence/ProductoRepository.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import mx.edu.tecdesoftware.market_backend.persistence.crud.ProductoCrudReposito
 import mx.edu.tecdesoftware.market_backend.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
 
@@ -14,4 +15,34 @@ public class ProductoRepository {
         //Se "castear" de Iterable a la lista
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    /*
+        SELECT * FROM productos
+        WHERE cantidad_stock < ?
+        AND estado = true;
+     */
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    //Obtener un producto dado por id
+    public Optional<Producto> getProductoById(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //Guardar un producto
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    //Eliminar por id
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 }
